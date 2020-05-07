91c018c0024fd2b31c1b43af991560d62ed8e6ce
Alejandro Jimenez <alejandro@weare8.com>
2020-05-07 15:18:36 -0600

    initial commit

---
+++ src/components/Homepage/Homepage.jsx
@@ -0,0 +1,20 @@
+//react
+import React from "react";
+
+//components styles and assets
+import "./homepage.scss";
+
+const Homepage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Homepage;
